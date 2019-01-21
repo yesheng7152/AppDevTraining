d8e078eb60b72400220157cfe947a73472d572d3
YeshengC <yesheng250416@gmail.com>
2019-01-20 22:15:13 -0600

    started cat table view and cell

---
+++ Toy/Toy/CatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CatTableViewCell.swift
+//  Toy
+//
+//  Created by Yesheng Chen on 1/20/19.
+//  Copyright © 2019 Yesheng Chen. All rights reserved.
+//
+
+import UIKit
+
+class CatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Toy/Toy/ViewController.swift
@@ -0,0 +1,77 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var submitted :Bool = false
+
+    @IBOutlet weak var Classtext: UITextView!
+    
+    @IBOutlet weak var ClassYearTextField: UITextField!
+    
+    @IBOutlet weak var Nametext: UITextView!
+  
+    @IBOutlet weak var NameTextField: UITextField!
+    
+    @IBOutlet weak var SubmitButton: UIButton!
+    
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
+    func enterClearMode() -> Void {
+        Nametext.isHidden = true
+        Classtext.isHidden = true
+        NameTextField.isHidden = true
+        ClassYearTextField.isHidden = true
+        DisplayLabel.isHidden = false
+        SubmitButton.setTitle("Clear", for: .normal)
+        submitted = true
+    }
+    
+    func enterSubmitMode() -> Void {
+        Nametext.isHidden = false
+        Classtext.isHidden = false
+        NameTextField.isHidden = false
+        ClassYearTextField.isHidden = false
+        DisplayLabel.isHidden = true
+        SubmitButton.setTitle("Submit", for: .normal)
+        submitted = false
+    }
+    
+    @IBAction func SubmitButtonTapped(_ sender: Any) {
+        if (submitted){
+            UserDefaults.standard.removeObject(forKey: "name")
+            UserDefaults.standard.removeObject(forKey: "class")
+            enterSubmitMode()
+        }else{
+            let name = NameTextField.text ?? ""
+            let classYr = ClassYearTextField.text ?? ""
+            DisplayLabel.text = "Welcome " + name + " of " + classYr + "!"
+            NameTextField.text = ""
+            ClassYearTextField.text = ""
+            UserDefaults.standard.set(name, forKey:"name")
+            UserDefaults.standard.set(classYr, forKey:"class")
+            enterClearMode()
+        }
+    }
+    
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if (UserDefaults.standard.object(forKey: "name") != nil){
+            let name = UserDefaults.standard.string(forKey: "name")
+            let classYr = UserDefaults.standard.string(forKey: "class")
+            DisplayLabel.text = "Welcome, " + name! + "of " + classYr! + "!"
+            enterClearMode()
+        }else{
+            enterSubmitMode()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
